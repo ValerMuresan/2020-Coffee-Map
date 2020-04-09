8f75a5c9dfc0ee0b3b54cf764304424fcff7982d
Valer-Muresan <muresanvaler86@yahoo.com>
2020-04-09 22:08:28 +0300

    Updates

---
+++ precache-manifest.06495fb5f17000eb9640f377f9414e2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfba58eb704e2eda4f9066eca4ec469",
+    "url": "/2020-coffee-map/index.html"
+  },
+  {
+    "revision": "a30e5b440ed0351cc806",
+    "url": "/2020-coffee-map/static/css/main.50210a22.chunk.css"
+  },
+  {
+    "revision": "6c45b84e657974be8707",
+    "url": "/2020-coffee-map/static/js/2.3718fcf3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/2020-coffee-map/static/js/2.3718fcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30e5b440ed0351cc806",
+    "url": "/2020-coffee-map/static/js/main.2f6f0c04.chunk.js"
+  },
+  {
+    "revision": "1225620bfba2d17b3c65",
+    "url": "/2020-coffee-map/static/js/runtime-main.3d111044.js"
+  },
+  {
+    "revision": "8f75779aaefa7b0d4f2c780be0db6d1a",
+    "url": "/2020-coffee-map/static/media/udacity.8f75779a.png"
+  }
+]);
